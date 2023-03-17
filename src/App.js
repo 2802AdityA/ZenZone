5a3dae30d3fc43c9db5b9b476916e0163e243bf4
Mahikagarg09 <mahika_garg@yahoo.com>
2023-03-17 18:24:00 +0530

    video call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage';
-// import Video from './components/Video/Video'
+import Video from './components/Video/Video'
 import Form from './components/form/Form'
 import Appointment from './pages/Appointment';
 
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route path="/find-counsellors" element={<Appointment />} />
+        <Route path="/video" element={<Video />} />
         <Route path="/form" element={<Form />} />
       </Routes>
     </Router>
--- src/components/appointment/Appointment.js
+++ src/components/appointment/Appointment.js
@@ -3,6 +3,7 @@ import Img1 from '../../assets/acard-1.jpg'
 import Img2 from '../../assets/acard-2.jpeg'
 import Img3 from '../../assets/acard3.jpg'
 import useStyles from './AppointmentStyle'
+import { useNavigate } from "react-router-dom";
 
 const Appointment = () => {
 
@@ -23,12 +24,15 @@ const Appointment = () => {
         }
     ];
 
+    let navigate = useNavigate();
+
+
     return (
         <div>
             <div className={classes.cardSection}>
                 {cardData.map((card) => (
                     <div key={card.name}>
-                        <img src={card.image} alt={card.name}/>
+                        <img src={card.image} alt={card.name} onClick={() => navigate('/video')}/>
                         <h3>{card.name}</h3>
                     </div>
                 ))}
