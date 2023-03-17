15186d44426774f57d1d70eeeb43ec88674f2b5a
muskan garg <muskangarg02270@gmail.com>
2023-03-17 11:53:34 +0530

    Added app.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FormPage from "./pages/FormPage";
+// import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <>
+    <FormPage/>
+  </>
+  );
+}
+
+export default App;
