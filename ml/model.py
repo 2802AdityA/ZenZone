2bddf799dc24388842d6820494868d3197480c5f
Menma <ljivesh@gmail.com>
2023-03-17 09:24:08 +0530

    Add model

---
+++ ml/model.py
@@ -0,0 +1,218 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+from scipy.stats import randint
+# prep
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.datasets import make_classification
+from sklearn.preprocessing import binarize, LabelEncoder, MinMaxScaler
+# models
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+# Validation libraries
+from sklearn import metrics
+from sklearn.metrics import accuracy_score, mean_squared_error, precision_recall_curve
+from sklearn.model_selection import cross_val_score
+# #Neural Network
+# from sklearn.neural_network import MLPClassifier
+# from sklearn.model_selection import RandomizedSearchCV
+
+# #Bagging
+from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier
+# from sklearn.neighbors import KNeighborsClassifier
+
+# #Naive bayes
+# from sklearn.naive_bayes import GaussianNB 
+
+
+train_df = pd.read_csv('./survey.csv')
+print(train_df.head())
+
+#Pandas: whats the data row count?
+print(train_df.shape)
+    
+#Pandas: whats the distribution of the data?
+print(train_df.describe())
+    
+#Pandas: What types of data do i have?
+print(train_df.info())
+
+total = train_df.isnull().sum().sort_values(ascending=False)
+percent = (train_df.isnull().sum()/train_df.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+print(missing_data)
+
+#dealing with missing data
+#Let’s get rid of the variables "Timestamp",“comments”, “state” just to make our lives easier.
+train_df = train_df.drop(['comments'], axis= 1)
+train_df = train_df.drop(['state'], axis= 1)
+train_df = train_df.drop(['Timestamp'], axis= 1)
+
+train_df.isnull().sum().max() #just checking that there's no missing data missing...
+train_df.head(5)
+
+
+# Assign default values for each data type
+defaultInt = 0
+defaultString = 'NaN'
+defaultFloat = 0.0
+
+# Create lists by data tpe
+intFeatures = ['Age']
+stringFeatures = ['Gender', 'Country', 'self_employed', 'family_history', 'treatment', 'work_interfere',
+                 'no_employees', 'remote_work', 'tech_company', 'anonymity', 'leave', 'mental_health_consequence',
+                 'phys_health_consequence', 'coworkers', 'supervisor', 'mental_health_interview', 'phys_health_interview',
+                 'mental_vs_physical', 'obs_consequence', 'benefits', 'care_options', 'wellness_program',
+                 'seek_help']
+floatFeatures = []
+
+# Clean the NaN's
+for feature in train_df:
+    if feature in intFeatures:
+        train_df[feature] = train_df[feature].fillna(defaultInt)
+    elif feature in stringFeatures:
+        train_df[feature] = train_df[feature].fillna(defaultString)
+    elif feature in floatFeatures:
+        train_df[feature] = train_df[feature].fillna(defaultFloat)
+    else:
+        print('Error: Feature %s not recognized.' % feature)
+train_df.head(5)
+
+#clean 'Gender'
+#Slower case all columm's elements
+gender = train_df['Gender'].str.lower()
+#print(gender)
+
+#Select unique elements
+gender = train_df['Gender'].unique()
+
+#Made gender groups
+male_str = ["male", "m", "male-ish", "maile", "mal", "male (cis)", "make", "male ", "man","msle", "mail", "malr","cis man", "Cis Male", "cis male"]
+trans_str = ["trans-female", "something kinda male?", "queer/she/they", "non-binary","nah", "all", "enby", "fluid", "genderqueer", "androgyne", "agender", "male leaning androgynous", "guy (-ish) ^_^", "trans woman", "neuter", "female (trans)", "queer", "ostensibly male, unsure what that really means"]           
+female_str = ["cis female", "f", "female", "woman",  "femake", "female ","cis-female/femme", "female (cis)", "femail"]
+
+for (row, col) in train_df.iterrows():
+
+    if str.lower(col.Gender) in male_str:
+        train_df['Gender'].replace(to_replace=col.Gender, value='male', inplace=True)
+
+    if str.lower(col.Gender) in female_str:
+        train_df['Gender'].replace(to_replace=col.Gender, value='female', inplace=True)
+
+    if str.lower(col.Gender) in trans_str:
+        train_df['Gender'].replace(to_replace=col.Gender, value='trans', inplace=True)
+
+#Get rid of bullshit
+stk_list = ['A little about you', 'p']
+train_df = train_df[~train_df['Gender'].isin(stk_list)]
+
+print(train_df['Gender'].unique())
+
+
+#complete missing age with mean
+train_df['Age'].fillna(train_df['Age'].median(), inplace = True)
+
+# Fill with media() values < 18 and > 120
+s = pd.Series(train_df['Age'])
+s[s<18] = train_df['Age'].median()
+train_df['Age'] = s
+s = pd.Series(train_df['Age'])
+s[s>120] = train_df['Age'].median()
+train_df['Age'] = s
+
+#Ranges of Age
+train_df['age_range'] = pd.cut(train_df['Age'], [0,20,30,65,100], labels=["0-20", "21-30", "31-65", "66-100"], include_lowest=True)
+
+#There are only 0.014% of self employed so let's change NaN to NOT self_employed
+#Replace "NaN" string from defaultString
+train_df['self_employed'] = train_df['self_employed'].replace([defaultString], 'No')
+print(train_df['self_employed'].unique())
+
+#There are only 0.20% of self work_interfere so let's change NaN to "Don't know
+#Replace "NaN" string from defaultString
+
+train_df['work_interfere'] = train_df['work_interfere'].replace([defaultString], 'Don\'t know' )
+print(train_df['work_interfere'].unique())
+
+#Encoding data
+labelDict = {}
+for feature in train_df:
+    le = preprocessing.LabelEncoder()
+    le.fit(train_df[feature])
+    le_name_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+    train_df[feature] = le.transform(train_df[feature])
+    # Get labels
+    labelKey = 'label_' + feature
+    labelValue = [*le_name_mapping]
+    labelDict[labelKey] =labelValue
+    
+for key, value in labelDict.items():     
+    print(key, value)
+
+#Get rid of 'Country'
+train_df = train_df.drop(['Country'], axis= 1)
+train_df.head()
+
+#missing data
+total = train_df.isnull().sum().sort_values(ascending=False)
+percent = (train_df.isnull().sum()/train_df.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+print(missing_data)
+
+# Scaling Age
+scaler = MinMaxScaler()
+train_df['Age'] = scaler.fit_transform(train_df[['Age']])
+train_df.head()
+
+# define X and y
+feature_cols = ['Age', 'Gender', 'family_history', 'benefits', 'care_options', 'anonymity', 'leave', 'work_interfere']
+X = train_df[feature_cols]
+y = train_df.treatment
+
+# split X and y into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+# Create dictionaries for final graph
+# Use: methodDict['Stacking'] = accuracy_score
+methodDict = {}
+rmseDict = ()
+
+
+def boosting():
+    # Building and fitting 
+    clf = DecisionTreeClassifier(criterion='entropy', max_depth=1)
+    boost = AdaBoostClassifier(base_estimator=clf, n_estimators=500)
+    boost.fit(X_train, y_train)
+    
+    # make class predictions for the testing set
+    y_pred_class = boost.predict(X_test)
+    
+    print('########### Boosting ###############')
+
+    
+    
+    # accuracy_score = evalClassModel(boost, y_test, y_pred_class, True)
+
+    #Data for final graph
+    methodDict['Boosting'] = accuracy_score * 100
+
+# Generate predictions with the best method
+clf = AdaBoostClassifier()
+clf.fit(X, y)
+dfTestPredictions = clf.predict(X_test)
+
+# Write predictions to csv file
+# We don't have any significative field so we save the index
+results = pd.DataFrame({'Index': X_test.index, 'Treatment': dfTestPredictions})
+# Save to file
+# This file will be visible after publishing in the output section
+results.to_csv('results.csv', index=False)
+print(results.head(10))
+
+print(X.head(10))
